147f72d3e24ec3b7a0c7afd37427be47db6bb979
The SunShining <thesunshiningmin@gmail.com>
2024-10-31 07:22:05 -0600

    Create sync_all_to_here.sh

---
+++ sh/sync_all_to_here.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# SYNC ALL DOTFILES TO THIS REPO BY RUNNING ALL SH FILES!!!
+
+./zsh_copy_to_dotfiles.sh
+./kitty_copy_to_dotfiles.sh
+./vimrc_copy_to_dotfiles.sh
+
+echo "SYNC successful!"
